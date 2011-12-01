56801d42e75ca423b9916e2eb99c680e37931faf
Nik Cubrilovic <nikcub@gmail.com>
2011-12-02 00:59:41 +1100

    issues #2 #3 #4

--- content.js
+++ content.js
@@ -26,24 +26,38 @@
 
 // @TODO need to detect only the boxes that close and redirect here
 // @TODO not closing some dialogs (The Independant)
+
+// 1. Auto-close dialogs
 var v = document.getElementsByName('cancel_clicked');
+// console.info('cancel_clicked', v);
 if(v.length) {
   console.info('frictionless:cancel_clicked:', v[0]);
   v[0].click();
 }
 
 // @TODO rewrite a href's so that the dialog doesn't appear (remove rel=dialog)
-
+// <a data-appname="Yahoo!" href="/connect/uiserver.php?app_id=194699337231859&amp;method=permissions.request&amp;redirect_uri=http%3A%2F%2Fnews.yahoo.com%2Ftech-firm-implements-employee-zero-email-policy-165311050.html%3Ffb_action_ids%3D10150433450240238%252C971018054873%252C732881153264%252C10100288001030476%252C10150417559698820%26fb_action_types%3Dnews.reads%26fb_source%3Dother_multiline&amp;response_type=code&amp;display=async&amp;perms=email%2Cpublish_actions%2Cuser_birthday%2Cuser_likes&amp;auth_referral=1" rel="dialog" title="Tech Firm Implements Employee ‘Zero Email’ Policy" class="">Tech Firm Implements Employee ‘Zero Email’ Policy</a>
+// 2. Cancel dialogs
 var d_els = document.querySelectorAll("a[data-appname][rel='dialog']");
 for(var x=0; x < d_els.length; x++) {
   var n = d_els.item(x);
-  var norel = document.createAttribute('rel');
-  norel.value = '';
-  n.attributes.setNamedItem(norel);
+  n.removeAttribute('rel');
+  n.removeAttribute('onmousedown');
+  n.setAttribute('target', '_blank');
+  console.info('rewrite:', n);
+  rewrite_link(n);
 }
-document.body.addEventListener("click", parse_links);
 
+function rewrite_link(el) {
+  var params = get_params(el.href);
+  if('redirect_uri' in params) {
+    console.info('link_rewrite:', el, params['redirect_uri']);
+    el.setAttribute('href', params['redirect_uri']);
+  }
+}
 
+// 3. Parse link clicks that have the URL in params
+// document.body.addEventListener("click", parse_links);
 function parse_links(ev) {
   if(ev.target && ev.target.nodeName == 'A' && ev.target.pathname == '/connect/uiserver.php') {
     var params = get_params(ev.target.href);
